e126767dc75e30bca7655c154e3b64d177104cb0
Tnuji <tunjioluwatoki@gmail.com>
2026-09-26 08:02:47 -0500

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int answer = nums.length;
+
+        for(int i = 0; i < nums.length; ++i)
+        {
+            answer ^= i;
+            answer ^= nums[i];
+        }
+
+        return answer;
+    }
+}
